f93942eb575aa0e56a9d9a638ef8d5f35dbd060f
Yutong Pan <yutonp@uw.edu>
2015-12-06 12:33:58 -0800

    flower

--- main.js
+++ main.js
@@ -4,11 +4,6 @@ var mic;
 // Set up cannot be named differently, constructs canvas
 function setup() {
   createCanvas(displayWidth, displayHeight);
-
-  // microphone volume input
-  mic = new p5.AudioIn();
-  mic.start();
-
 }
 
 // draw cannot be named differently, will be called in a loop
